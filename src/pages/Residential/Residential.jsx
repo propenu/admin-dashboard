f2456c7a6c80b9995347edb7b94e1058bee5703e
dattavenkataramana <bavirisettidvramana@gmail.com>
2026-03-24 11:27:55 +0530

    24-03-2026

--- src/pages/Residential/Residential.jsx
+++ src/pages/Residential/Residential.jsx
@@ -259,7 +259,7 @@ export default function Commercial() {
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
         <div>
           <h1 className="text-3xl font-bold text-[#27AE60] max-sm:text-xl max-sm:text-center">
-            Residential Properties
+            Residential Properties adtta
           </h1>
           <p className="text-slate-600 mt-1 max-sm:text-center">
             Explore residential property listings.
