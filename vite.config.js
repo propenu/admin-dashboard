d0159f93bdd35798781ec41f27ca902e86c2899c
dattavenkataramana <bavirisettidvramana@gmail.com>
2026-04-21 10:25:17 +0530

    updated prices on application

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+
+  // 🔥 VERY IMPORTANT for Netlify
+  base: "/",
+
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
+
+  server: {
+    port: 5173,
+  },
+});
