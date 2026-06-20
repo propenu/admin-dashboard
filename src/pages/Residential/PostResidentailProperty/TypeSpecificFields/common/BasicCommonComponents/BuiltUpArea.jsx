918179817cec34e625489e7bde4457f0d1c6f4f6
dattavenkataramana <bavirisettidvramana@gmail.com>
2026-06-20 17:23:56 +0530

    in featured card add reneval

--- src/pages/Residential/PostResidentailProperty/TypeSpecificFields/common/BasicCommonComponents/BuiltUpArea.jsx
+++ src/pages/Residential/PostResidentailProperty/TypeSpecificFields/common/BasicCommonComponents/BuiltUpArea.jsx
@@ -8,6 +8,7 @@ export const BuiltUpArea = forwardRef(({ error }, ref) => {
   const { form, updateFieldValue } = useActivePropertySlice();
   return (
     <div ref={ref} className="space-y-2">
+      
       <div className="flex items-center gap-2">
         <p className="text-xs font-bold text-[#374151] uppercase tracking-wide">
           Built-up Area
@@ -24,24 +25,11 @@ export const BuiltUpArea = forwardRef(({ error }, ref) => {
       <div
         className={`flex border-2 rounded-xl overflow-hidden bg-white transition-all ${error ? "border-red-300" : "border-[#e5e7eb] focus-within:border-[#27AE60] focus-within:ring-2 focus-within:ring-[#27AE60]/10"}`}
       >
-        {/* <input
-          type="number"
-          placeholder="0"
-          value={form.builtUpArea || ""}
-          onChange={(e) => updateFieldValue("builtUpArea", e.target.value)}
-          className="flex-1 px-4 py-3 outline-none text-sm font-semibold placeholder:text-[#c9c9c9] text-[#111827]"
-        /> */}
         <input
           type="number"
           placeholder="0"
           value={form.builtUpArea || ""}
-          onChange={(e) => {
-            const value = e.target.value;
-
-            if (value === "" || Number(value) > 0) {
-              updateFieldValue("builtUpArea", value);
-            }
-          }}
+          onChange={(e) => updateFieldValue("builtUpArea", e.target.value)}
           className="flex-1 px-4 py-3 outline-none text-sm font-semibold placeholder:text-[#c9c9c9] text-[#111827]"
         />
         <div className="flex items-center px-2 bg-[#f9fafb] border-l border-[#e5e7eb] text-[#9ca3af] text-xs font-bold whitespace-nowrap">
